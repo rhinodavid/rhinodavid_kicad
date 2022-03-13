028a52adb231992f1c5a6b652c28c8fb6d59704b
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2022-03-12 23:03:06 -0700

    Create docs v0

--- docs/readme/generate.sh
+++ docs/readme/generate.sh
@@ -4,7 +4,8 @@ BASEDIR=$(dirname "$0")
 
 ECHO $BASEDIR
 
-hbs --data $BASEDIR/parts.json \
+hbs --data $BASEDIR/data.json \
+    --data $BASEDIR/configuration.json \
     --partial $BASEDIR/_main.hbs \
     --extension MD \
     $BASEDIR/README.hbs
